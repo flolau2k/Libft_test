b88f6233def06006bbc95c1fa2a3f15e2222e01c
flauer <flauer@student.42heilbronn.de>
2023-03-21 18:16:09 +0100

    base functions done

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 11:50:12 by flauer            #+#    #+#             */
-/*   Updated: 2023/03/17 14:27:36 by flauer           ###   ########.fr       */
+/*   Updated: 2023/03/21 17:37:41 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	if (dstlen > dstsize)
 		return (dstsize + ft_strlen(src));
 	i = 0;
-	while ((i + dstlen) < (dstsize - 1))
+	while (src[i] && (i + dstlen + 1) < dstsize)
 	{
 		dst[i + dstlen] = src[i];
 		i++;
