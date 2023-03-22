f8fcb0eb7909258bc96176aa131fc6fe30ab9cda
flauer <flauer@student.42heilbronn.de>
2023-03-22 19:42:31 +0100

    fix and wip

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 18:58:28 by flauer            #+#    #+#             */
-/*   Updated: 2023/03/21 19:01:32 by flauer           ###   ########.fr       */
+/*   Updated: 2023/03/22 19:29:25 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	t_list	*curr_elm;
 
+	if (!f || !lst)
+		return ;
 	curr_elm = lst;
 	while (curr_elm)
 	{
-		f(curr_elm);
+		f(curr_elm->content);
 		curr_elm = curr_elm->next;
 	}
 }
--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,28 +6,33 @@
 /*   By: flauer <flauer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 19:01:59 by flauer            #+#    #+#             */
-/*   Updated: 2023/03/22 17:28:27 by flauer           ###   ########.fr       */
+/*   Updated: 2023/03/22 19:42:17 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list	*ret;
-// 	t_list	*curr_elm;
 
-// 	if (!lst || !f)
-// 		return (NULL);
-// 	curr_elm = lst;
-// 	ret = ft_lstnew(f(curr_elm->content));
-// 	if (!ret)
-// 		return (NULL);
-// 	curr_elm = curr_elm->next;
-// 	while (curr_elm)
-// 	{
-// 		ft_lstadd_back(&ret, f(curr_elm->content));
-// 		curr_elm = curr_elm->next;
-// 	}
-// 	return (ret);
-// }
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*temp;
+	t_list	*ret;
+	t_list	*curr_elm_ret;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	ret = ft_lstnew(f(lst->content));
+	if (!ret)
+		return (NULL);
+	curr_elm_ret = ret;
+	while (curr_elm_ret)
+	{
+		lst = lst->next;
+		if (!lst->content)
+			ft_lstdelone(lst, del);
+		curr_elm_ret->next = ft_lstnew(f(lst->content));
+		curr_elm_ret = curr_elm_ret->next;
+	}
+	return (ret);
+}
